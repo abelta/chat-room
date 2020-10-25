0033629ff4fbc1228c367bb4804d56f5f16f0b39
Abel <abel@Abels-MacBook-Air.local>
2020-10-25 19:46:17 +0100

    Change in Header.

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default () => (
-    <h1 className="text-5xl font-bold tracking-wide text-gray-100">
+    <h1 className="text-5xl font-bold tracking-widest text-gray-100">
         CHAT
         <span className="text-red-400">
             !
